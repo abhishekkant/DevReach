994f0df837758e343921d71400adaca6a7547471
Dhananjay Kumar <dhananjay.kumar@live.com>
2013-09-05 22:56:55 -0500

    iOS release version

--- DevReach/scripts/default.js
+++ DevReach/scripts/default.js
@@ -4,9 +4,8 @@
 var everliveKey = 'DJOOCpPmPJe2fJ8s';
 var GoogleProjectID = '541570376695';
 var app;
-var dateData = [{"dateTitle":"Day 1", "dateValue":"8/24/2013"}, 
-    {"dateTitle":"Day 2", "dateValue":"8/25/2013"}, 
-    {"dateTitle":"Day 3", "dateValue":"8/26/2013"}
+var dateData = [{"dateTitle":"Day 1", "dateValue":"10/01/2013"}, 
+    {"dateTitle":"Day 2", "dateValue":"10/02/2013"}
 ];
 
 
@@ -933,6 +932,7 @@ function checkPersonalSettings() {
         $('#isAnonymous').data("kendoMobileSwitch").check(personalDetails[2].isAnonymous);
         $('#savePersonalDetails').text('Update');
     }
+ 
 }
 
 
@@ -1226,3 +1226,104 @@ function refreshAllSessionsData()
     localStorage.removeItem("venues");
     
 }
+
+
+/*function getParameterByName(name, url) {
+	name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
+	var regexS = name + "=([^&#]*)";
+ 			
+	console.log("Parameter name: " + name);
+	console.log("Url: " + url);
+            
+	var regex = new RegExp(regexS);
+	var results = regex.exec(url);
+            
+	console.log("Result: " + results);
+            
+	if (results == null) {
+		return false;
+	}
+	else 
+		return decodeURIComponent(results[1].replace(/\+/g, " "));
+}
+
+
+// https://developers.facebook.com/docs/facebook-login/login-flow-for-web-no-jssdk/
+
+var IdentityProvider = function (config) {
+	var that = this;
+    
+	this.getAccessToken = function(callback) {
+		console.log("Begin authorization with: " + config.providerName);
+		// Begin Authorization
+		var authorize_url = config.endpoint
+							+ "?response_type=" + config.response_type
+							+ "&client_id=" + config.client_id
+							+ "&redirect_uri=" + config.redirect_uri
+							+ "&display=" + config.display
+							+ "&access_type=" + config.access_type
+							+ "&scope=" + config.scope
+   
+		//CALL IN APP BROWSER WITH THE LINK
+		ref = window.open(authorize_url, '_blank', 'location=no');
+		console.log(authorize_url)
+        
+		console.log("Attaching loadstart event")
+		ref.addEventListener('loadstart', function(event) {
+			console.log("loadstart event handler")
+			//that.facebookLocChanged(event.url, callback);
+		});
+        
+		console.log("Attaching loadstop event")
+		ref.addEventListener('loadstop', function(event) {
+			console.log("loadstop event handler")
+			that.locationChanged(event.url, callback);
+		});
+	}
+    
+	this.locationChanged = function(loc, callback) {
+		console.log("Current location: " + loc)
+		if (loc.indexOf("access_token=") != -1) {
+			ref.close();
+			console.log(loc);
+			var token = getParameterByName("access_token", loc);
+            
+            callback(token);
+		} else {
+            console.log("No access_token in url.")
+        }
+	}
+}
+
+
+
+function fbLogin()
+{
+    var facebookConfig = {
+			name: "Facebook",
+			loginMethodName: "loginWithFacebook",
+			endpoint: "https://www.facebook.com/dialog/oauth",
+			response_type:"token",
+			client_id: "622842524411586",
+			redirect_uri:"https://www.facebook.com/connect/login_success.html",
+			access_type:"online",
+			scope:"email",
+			display: "touch"
+		};
+		var facebook = new IdentityProvider(facebookConfig); 
+    
+    facebook.getAccessToken(function(token) {
+				Everlive.$.Users.loginWithFacebook(token)
+				.then(function(res) {
+					console.log(res);
+					var message = "Welcome to Everlive!";
+					navigator.notification.alert(message, function() {
+					}, "Everlive")
+				}, function(err) {
+					console.log(err);
+					navigator.notification.alert(err.message, function() {
+					}, "Everlive")
+				})
+			})
+		
+}*/
